a7da4be5a3ff0f3e436b50b97c97fc65ab1e5f76
CrackerTeg <crackerteg@gmail.com>
2015-02-16 11:45:01 -0600

    Broke it
    Signed-off-by: CrackerTeg <crackerteg@gmail.com>

--- broken.mk
+++ broken.mk
@@ -1,20 +1,26 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/broken/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := hammerhead
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := cm_hammerhead
+PRODUCT_NAME := broken_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
