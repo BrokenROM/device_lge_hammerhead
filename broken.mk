44d5bf3018ec7440d03b3ade2ead76db6dfc3453
CrackerTeg <crackerteg@gmail.com>
2016-08-10 16:47:58 -0500

    Update fingerprint to MOB30Y
    Signed-off-by: CrackerTeg <crackerteg@gmail.com>

--- broken.mk
+++ broken.mk
@@ -24,8 +24,8 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30D/2704746:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30D 2704746 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30Y/3067468:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30Y 3067468 release-keys"
 
 # Broken Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
